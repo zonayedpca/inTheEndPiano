878397bff8fa37f6fdbf4e7dbe98d1b7ff9b28cf
Zonayed Ahmed <zonayedpca@yahoo.com>
2017-10-19 08:58:04 +0530

    trubute to chester

---
+++ main.js
@@ -0,0 +1,21 @@
+const keys = document.querySelectorAll('.key');
+
+const soundPlay = (e) => {
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    
+    if(!sound) return;
+    
+    sound.currentTime = 0;
+    sound.play();
+    
+    key.classList.add('playing');
+}
+
+const removeStyle = (e) => {
+    e.target.classList.remove('playing');
+}
+
+window.addEventListener('keydown', soundPlay);
+
+keys.forEach(key=>addEventListener('transitionend', removeStyle));
